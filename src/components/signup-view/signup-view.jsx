cbd484f9659ef4f87e5d676bb9330e401bbf7d85
Zachariah M Ellis <zachariah.m.ellis@gmail.com>
2026-01-21 19:02:38 -0700

    Add signup view and display login/signup for unauthenticated users

---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+
+export const SignupView = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday,
+    };
+
+    fetch("https://cryptic-lowlands-83913-a6a2dd7d9144.herokuapp.com/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Signup successful! Please log in.");
+          window.location.reload();
+        } else {
+          alert("Signup failed");
+        }
+      })
+      .catch(() => {
+        alert("Something went wrong");
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="5"
+        />
+      </label>
+
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        Birthday:
+        <input
+          type="date"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+        />
+      </label>
+
+      <button type="submit">Register</button>
+    </form>
+  );
+};
